41cd116920b8c45e47b188b97dda694eddae1e54
Anderson <andersecng@gmail.com>
2017-09-02 17:00:15 -0400

    Refactor gamblebot

--- gamblebot.py
+++ gamblebot.py
@@ -1,8 +1,10 @@
 import sys
 import time
+from threading import Timer
 from os import environ
 from slackclient import SlackClient
 from gamblegame import GambleGame
+from gamblestate import GameState
 
 class GambleBot:
 
@@ -20,76 +22,44 @@ class GambleBot:
             if time.time() > timeout:
                 return -1
             
-            username, command, channel = self.parse_slack_output(self.slack_client.rtm_read())
-            if(command and channel):
-                if username != None and username == player and command.split()[1] == "roll":
+            username, msg, channel = self.parse_slack_output(self.slack_client.rtm_read())
+            if(msg and channel):
+                if username != None and username == player and msg.split()[1] == "roll":
                     return self.game.players[player].roll()
 
-    def get_bet_amount(self, initial_amount):
-        betted_users = []
-        timeout = time.time() + 30   # 30 seconds
-        while True:
-            if time.time() > timeout:
-                break
-            username, command, channel = self.parse_slack_output(self.slack_client.rtm_read())
-            if(command and channel):
-                command_split = command.split()
-                if username != None and len(command_split) >= 2 and command_split[1] == "bet":
-                    if (username in betted_users):
-                        response = username + " has already placed a bet"
-                    else:
-                        try:
-                            self.game.add_player(username)
-                            bet = self.game.players[username].bet(initial_amount)
-                            if type(bet) is str:
-                                response = username + " you do not have enough money to place that bet"
-                                self.game.remove_player(username)
-                            else:
-                                self.game.current_players[username] = bet
-                                betted_users.append(username)
-                                response = username + " placed a bet of " + str(initial_amount)
-                        except:
-                            response = "bet amount is not correct"
-
-                    self.post(response, channel)
-        self.post("Betting is now complete. Users who did not bet will be removed from current game", channel)
-        time.sleep(1)
-        '''for user in self.game.current_players.keys():
-            if user not in betted_users:
-                self.game.current_players.pop(user)'''
-
-        return sum(bet for bet in self.game.current_players.values()) #return sum of all bets
-
-    def handle_command(self, username, command, channel):
-        print(command)
+    def handle_command(self, username, msg, channel):
+        print(msg)
         response = ""
-        if len(command.split()) > 1:
-            gamble_command = command.split()[1]
+        if len(msg.split()) > 1:
+            gamble_command = msg.split()[1]
             if gamble_command == "start":
-                if len(command.split()) < 3:
+                if len(msg.split()) != 3:
                     response = "Please specify bet amount"
                 else:
-                    response = self.game.start()
+                    bet_amount = int(msg.split()[2])
+                    response = self.game.start(bet_amount)
                     if response == "":
-                        bet_amount = int(command.split()[2])
                         self.post(username + " has started a game. Amount to join is $" + str(bet_amount) +
                                   ".\n 30 seconds to bet, type 'bet' to place bet", channel)
-                        total_pot = self.get_bet_amount(bet_amount)
-                        if len(self.game.current_players) < 1:
-                            response = "Need more players"
-                            self.game.running = False
-                        else: 
-                            for player_name in self.game.players.keys():
-                                response = player_name + "'s turn to roll"
-                                self.post(response, channel)
-                                random_int = self.get_user_input(player_name)
-                                self.game.update_winner(player_name, random_int)
-                                response = "You rolled " + str(random_int)
-                                self.post(response, channel)
-                                time.sleep(1)
-                            response = "Winner of this round is " + self.game.winning_player_name + "\n"
-                            self.game.end(total_pot)
-                            response += self.game.list_score()
+                        t = Timer(30, self.game.start_rolling())
+                        t.start()
+
+            elif gamble_command == "bet" and self.game.state == GameState.BETTING:
+                try:
+                    self.game.add_player(username)
+                    bet = self.game.players[username].bet(self.game.bet_amount)
+                    if type(bet) is str:
+                        response = username + " you do not have enough money to place that bet"
+                        self.game.remove_player(username)
+                    else:
+                        self.game.current_players[username] = bet
+                        response = username + " placed a bet of " + str(self.game.bet_amount)
+                except:
+                    response = "bet amount is not correct"
+
+            elif gamble_command == "roll" and self.game.state == GameState.ROLLING:
+                response = self.game.roll(username)
+
             elif gamble_command == "list":
                 response = self.game.list_players()
 
@@ -120,9 +90,9 @@ class GambleBot:
         if self.slack_client.rtm_connect():
             print("Bot connected and running!")
             while True:
-                username, command, channel = self.parse_slack_output(self.slack_client.rtm_read())
-                if command and channel:
-                    self.handle_command(username, command, channel)
+                username, msg, channel = self.parse_slack_output(self.slack_client.rtm_read())
+                if msg and channel:
+                    self.handle_command(username, msg, channel)
         else:
             print("Connection failed.")
 
