618f6a17b090f8790c3c9ea50c5192d2bfbdc060
Anderson <andersecng@gmail.com>
2017-10-12 19:59:26 -0400

    Add tests

---
+++ tests/test_game.py
@@ -0,0 +1,12 @@
+import unittest
+from bot.gamblegame import GambleGame
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.game = GambleGame()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- tests/test_player.py
+++ tests/test_player.py
@@ -1,4 +1,42 @@
 import unittest
-from bot.gambleplayer import player
+from bot.gambleplayer import Player
 
 class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.player = Player("John")
+        self.other_player = Player("Appleseed", 200)
+
+    def test_default_param(self):
+        self.assertEqual(self.player.amount, 100)
+
+    def test_optional_param(self):
+        self.assertEqual(self.other_player.amount, 200)
+
+    def test_roll(self):
+        self.assertTrue(type(self.player.roll() == int))
+
+    def test_bet_negative(self):
+        self.assertEqual(self.player.bet(-1), 0)
+
+    def test_bet_greater(self):
+        self.assertEqual(self.player.bet(200), 0)
+
+    def test_bet(self):
+        self.assertEqual(self.player.bet(10), 10)
+
+    def test_gift_negative(self):
+        self.assertEqual((self.player.gift(-10, self.other_player)), 0)
+
+    def test_gift_greater(self):
+        self.assertEqual((self.player.gift(200, self.other_player)), 0)
+
+    def test_gift(self):
+        self.assertEqual((self.player.gift(10, self.other_player)), 10)
+        self.assertEqual(self.other_player.amount, 210)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
